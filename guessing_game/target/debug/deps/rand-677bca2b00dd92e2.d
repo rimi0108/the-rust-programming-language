44e50f0352d10f0097757785a8dbe921b75ac8ed
최혜림 <choehyerim@choehyelim-ui-iMac.local>
2022-03-28 14:06:38 +0900

    1, 2

--- guessing_game/target/debug/deps/rand-677bca2b00dd92e2.d
+++
@@ -1,9 +0,0 @@
-/Users/choehyerim/Desktop/the-rust-programming-language/guessing_game/target/debug/deps/rand-677bca2b00dd92e2.rmeta: /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/choehyerim/Desktop/the-rust-programming-language/guessing_game/target/debug/deps/librand-677bca2b00dd92e2.rlib: /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/choehyerim/Desktop/the-rust-programming-language/guessing_game/target/debug/deps/rand-677bca2b00dd92e2.d: /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
-/Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
-/Users/choehyerim/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/choehyerim/Desktop/the-rust-programming-language/guessing_game/target/debug/guessing_game: /Users/choehyerim/Desktop/the-rust-programming-language/guessing_game/src/main.rs
